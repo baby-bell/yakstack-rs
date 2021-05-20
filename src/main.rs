c754d6a08d40d6713362bd08bc6ddc9e53f1cceb
Brose Johnstone <baby-bell@users.noreply.github.com>
2021-05-19 17:38:32 -0700

    Require subcommands

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::cmp;
 use rusqlite::{Connection, OptionalExtension};
 use rusqlite::params;
 use rusqlite::Result as RusqliteResult;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App, SubCommand, AppSettings};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("yakstack")
                       .version("0.1")
                       .about("yak-shaving stack")
+                      .settings(AppSettings::SubcommandRequiredElseHelp)
                       .subcommand(SubCommand::with_name("push")
                           .about("Push a task onto the stack")
                           .arg(Arg::with_name("TASK")
@@ -33,7 +34,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     db_path.push("yakstack.db");
     let conn = Connection::open(db_path)
                           .map_err(|e| format!("unable to open yakstack database: {}", e))?;
-    conn.execute("CREATE TABLE IF NOT EXISTS tasks(task TEXT NOT NULL, task_order INTEGER PRIMARY KEY)", [])?;
+    conn.execute("CREATE TABLE IF NOT EXISTS tasks(task TEXT NOT NULL, task_order REAL NOT NULL, id INTEGER PRIMARY KEY, CHECK (task_order = task_order))", [])?;
     match matches.subcommand() {
         ("push", submatches) => {
             let task = submatches.unwrap().value_of("TASK").unwrap();
@@ -53,10 +54,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         ("clear", _) => clear_tasks(&conn)?,
         ("ls", _) => list_tasks(&conn)?.iter().enumerate().for_each(|(i, task)| println!("{}. {}", i, task)),
-        _ => {
-            eprintln!("Must specify a subcommand: push, pop, ls, clear");
-            std::process::exit(1);
-        }
+        _ => unreachable!("No subcommand provided")
     }
     Ok(())
 }
